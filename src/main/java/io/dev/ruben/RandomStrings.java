d21dd8eb676745f4ebe02f900e08483d152997ac
Ruben Lins Silva <rubenls@ciandt.com>
2024-09-09 14:57:35 -0300

    initial commit

---
+++ src/main/java/io/dev/ruben/RandomStrings.java
@@ -0,0 +1,25 @@
+package io.dev.ruben;
+
+import java.security.SecureRandom;
+
+public class RandomStrings {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final int DEFAULT_LENGTH = 10; // Comprimento padrão da string
+
+    public static String generateRandomString(int length) {
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(CHARACTERS.length());
+            sb.append(CHARACTERS.charAt(index));
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String randomString = generateRandomString(DEFAULT_LENGTH);
+        System.out.println("String aleatória gerada: " + randomString);
+    }
+}
